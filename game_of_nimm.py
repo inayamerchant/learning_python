b0b7fa60fb69e58860ee4b8b4f02d86737ee5f61
Inaya Merchant <132578012+inayamerchant@users.noreply.github.com>
2025-04-20 00:03:25 +0530

    Update game_of_nimm.py

--- game_of_nimm.py
+++ game_of_nimm.py
@@ -1,9 +1,14 @@
+import random
+
 stones = 20
 player = 1
+human_plays = 1
 
 def main():
     global stones
     global player
+    global human_plays
+    human_plays = choose_player(human_plays)
     while stones > 0:
         print("There are " + str(stones) + " stones left.")
         stones = no_of_stones(stones)
@@ -11,6 +16,13 @@ def main():
         print()
     finish_game()
 
+# allows user to choose whether they want to go first or second
+def choose_player(x):
+    number = int(input("Would you like to be player 1 or player 2? "))
+    while number not in (1,2):
+        number = int(input("Please enter 1 or 2: "))
+    x = number
+    return x
 
 # alternating players
 def change_turn(a):
@@ -23,9 +35,13 @@ def change_turn(a):
 # account of number of stones left
 def no_of_stones(x):
         global player
-        stones_removed = int(input("Player " + str(player) + " would you like to remove 1 or 2 stones? "))
-        while stones_removed not in (1,2):
-            stones_removed = int(input("Please enter 1 or 2: "))
+        if player == human_plays:
+            stones_removed = int(input("Player " + str(player) + " would you like to remove 1 or 2 stones? "))
+            while stones_removed not in (1,2):
+                stones_removed = int(input("Please enter 1 or 2: "))
+        else:
+            stones_removed = random.randint(1,2)
+            print("Player " + str(player) + " would you like to remove 1 or 2 stones? " + str(stones_removed))
         x -= stones_removed
         return x
 
